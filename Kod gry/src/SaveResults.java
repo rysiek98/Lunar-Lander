96a76b5c2142af826603e68cdbf9b22649821541
rysiek98 <52510889+rysiek98@users.noreply.github.com>
2020-06-19 18:27:40 +0200

    Projekt Gotowy

---
+++ Kod gry/src/SaveResults.java
@@ -0,0 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+/** Klasa zapisuje wynik gry do pliku*/
+
+public class SaveResults {
+
+    /**Metoda zapisująca wynik gracza do pilku txt
+     * @param  name nick gracza
+     * @param points zdobyte punkty
+     * @param diffLevel poziom trudności
+     */
+    public static void saveResult(String name, int points, String diffLevel) throws Exception {
+        FileWriter file = new FileWriter("TabelaWynikow.txt", UTF_8, true);
+        BufferedWriter out = new BufferedWriter(file);
+        out.write("Nick gracza: "+name+" uzyskane punkty: "+Integer.toString(points)+" poziom trudności: "+diffLevel+"\r\n");
+        out.close();
+    }
+}
